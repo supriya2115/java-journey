975e3480470d391e2bfb58789c62ac07b8e86e1c
supriya2115 <supriyagaikwad036@gmail.com>
2025-07-28 19:42:48 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,15 @@
+package S1;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		double P=100;
+		double t=30;
+		double r=50;
+		double si=(P*r*t)/100;
+		System.out.println(si);
+		
+
+	}
+
+}
