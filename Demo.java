e7f3b61543ea67695453c8184a52a4a206875f59
supriya2115 <supriyagaikwad036@gmail.com>
2025-07-28 19:21:24 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,19 @@
+package P1;
+
+public class Demo {
+	double len=10.23;
+	 double  breadth=20.10;
+	 double p;
+	 
+      
+	public static void main(String[] args) {
+		double len=10.23;
+		 double  breadth=20.10;
+		 double p;
+		 p=2*(len+breadth);
+        
+        System.out.println("Perimeter of rectangle is="+p);
+       
+	}
+
+}
