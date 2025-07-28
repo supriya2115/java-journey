e1734c5e9a27b091f79497c298dcbbf94966cc44
supriya2115 <supriyagaikwad036@gmail.com>
2025-07-28 19:55:05 +0530

    Add files via upload

---
+++ Pattern21.java
@@ -0,0 +1,16 @@
+package S1;
+
+public class Pattern21 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
